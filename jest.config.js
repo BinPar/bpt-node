055df38a2c68091b0716f3f5b44271b69004b814
Miguel Rodríguez <miguel@binpar.com>
2022-01-20 14:53:41 +0100

    chore: removed react eslint plugins and updated config files

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,9 @@ const testRegex = '(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$'
 module.exports = {
   testRegex: testRegex,
   transform: {
-    '^.+\\.jsx?$': require.resolve('babel-jest'),
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.js$': require.resolve('babel-jest'),
+    '^.+\\.ts$': 'ts-jest'
   },
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/prod_node_modules/'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleFileExtensions: ['ts', 'js'],
 };

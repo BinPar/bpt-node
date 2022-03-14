ca50ff71d597b74605f7a7b84545453f916b4d29
Marcos González Andrés <marcos@binpar.com>
2022-03-14 19:04:19 +0100

    feat!: new binflow

--- .github/workflows/create-rancher-project-if-not-exists.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-project=$1
-res=$(rancher project ls | awk '{print $2}' | grep "^${project}$")
-if [ -z "$res" ]; then
-    rancher project create $project
-fi
--- .github/workflows/rancher-move-namespace-if-not-in-project.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-namespace=$1
-projectId=$2
-res=$(rancher namespaces ls | awk '{print $2}' | grep "^${namespace}$")
-if [ -z "$res" ]; then
-    rancher namespaces move $namespace $projectId
-fi

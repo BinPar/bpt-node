4785120b331b025148e7b41b3f2d0765da872ba0
Marcos González Andrés <marcos@binpar.com>
2022-03-16 20:57:51 +0100

    fix: arms stuff

--- .github/workflows/create-k8s-if-not-exists.sh
+++ .github/workflows/create-k8s-if-not-exists.sh
@@ -4,5 +4,5 @@ environments=$1
 cd ./k8s/templates
 for environment in $environments
 do
-  rancher kubectl apply -f ../compiled/$environment.yml
+  kubectl apply -f ../compiled/$environment.yml
 done

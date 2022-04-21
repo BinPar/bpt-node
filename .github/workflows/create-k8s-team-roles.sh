35b8cdb64de6ecadae1245208f30ad33a00bb2ea
Marcos González Andrés <marcos@binpar.com>
2022-04-21 15:23:01 +0200

    remove clusterrolebinding

--- .github/workflows/create-k8s-team-roles.sh
+++ .github/workflows/create-k8s-team-roles.sh
@@ -5,5 +5,4 @@ teams=$2
 repoOwner=$3
 for team in $teams; do
   kubectl create rolebinding $team-read-all --group="$repoOwner:$team" --clusterrole=cluster-read-all -n $namespace --save-config --dry-run=client -o yaml | kubectl apply -f -
-  kubectl create clusterrolebinding $team-read-ns --group="$repoOwner:$team" --clusterrole=cluster-read-namespaces --save-config --dry-run=client -o yaml | kubectl apply -f -
 done
